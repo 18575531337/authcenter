73b9f376b3ea957f890dd236a415e22da2b2bb5c
zhangjunfei <zhangjunfei@haizhi.com>
2017-10-13 10:55:14 +0800

    201710131054-解决循环引用

--- src/main/java/com/haizhi/authcenter/config/SecurityConfig.java
+++ src/main/java/com/haizhi/authcenter/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.haizhi.authcenter.config;
 
+import com.haizhi.authcenter.realm.UserRealm;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
@@ -10,14 +11,20 @@ import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.util.Factory;
 import org.apache.shiro.web.servlet.SimpleCookie;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 /**
  * Created by haizhi on 2017/10/9.
  */
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements ApplicationContextAware{
+
+    private ApplicationContext applicationContext;
 
     /**/
     @Bean("shiroFilter")
@@ -28,7 +35,16 @@ public class SecurityConfig {
                 new IniSecurityManagerFactory("classpath:shiro.ini");
         shiroFilterFactoryBean.setSecurityManager(factory.getInstance());
         */
+        shiroFilterFactoryBean.setSecurityManager(securityManager());
         return shiroFilterFactoryBean;
+    }
+
+    @Bean("securityManager")
+    @DependsOn("userRealm")
+    public SecurityManager securityManager(){
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(this.applicationContext.getBean(UserRealm.class));
+        return securityManager;
     }
 
     //session生命周期管理器
@@ -37,8 +53,6 @@ public class SecurityConfig {
         LifecycleBeanPostProcessor lifecycle = new LifecycleBeanPostProcessor();
         return lifecycle;
     }
-
-
 
     //ID 生成器
     @Bean
@@ -59,17 +73,16 @@ public class SecurityConfig {
         return sessionManager;
     }
 
-    @Bean("securityManager")
-    public SecurityManager securityManager(){
-        DefaultSecurityManager securityManager = new DefaultSecurityManager();
-        return securityManager;
-    }
-
     @Bean
     public SimpleCookie simpleCookie(){
         SimpleCookie simpleCookie = new SimpleCookie();
         simpleCookie.setHttpOnly(true);
         simpleCookie.setMaxAge(180000);
         return simpleCookie;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
